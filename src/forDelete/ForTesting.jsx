d1c751eb92f4622b541c0382aa07c2a7b0b0273c
Deshmukh Danish <dd39775@gmail.com>
2024-07-19 18:45:32 +0530

    add cloudinary library and add some cofiugation

--- src/forDelete/ForTesting.jsx
+++ src/forDelete/ForTesting.jsx
@@ -1,189 +1,131 @@
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  useWindowDimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import {AdvancedImage, upload} from 'cloudinary-react-native';
+import {cld} from '../service/cloudinary';
+import {launchImageLibrary} from 'react-native-image-picker';
 import {moderateScale} from 'react-native-size-matters';
 
 const ForTesting = () => {
-    const code = `public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
+  const {width} = useWindowDimensions();
+  const myImage = cld.image('samples/chair');
 
-          thread.start();
+  // Add image states and functions
+  const [image, setImage] = useState(null);
+  const [imageDetail, setImageDetails] = useState(null);
+  console.log(imageDetail);
+  const openImagePicker = async () => {
+    const options = {
+      title: 'Select Image',
+      type: 'library',
+      options: {
+        mediaType: 'photo',
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+        selectionLimit: 1,
+        quality: 0.5,
+      },
+    };
 
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
+    const image = await launchImageLibrary(options);
+    let imageUri = image.uri || image.assets?.[0]?.uri;
 
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
+    if (image.didCancel === true) {
+      setImage(null);
+      setImageDetails(null);
+    } else {
+      setImage(imageUri);
+      setImageDetails(image);
+    }
+  };
 
-          thread.start();
+  const uploadImage = async () => {
+    if (!imageDetail) {
+      return;
+    }
 
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
+    const options = {
+      upload_preset: 'Default',
+      unsigned: true,
+    };
 
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-      
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
-
-          thread.start();
-
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
-
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-
-
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
-
-          thread.start();
-
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
-
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-
-
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
-
-          thread.start();
-
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
-
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-
-
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
-
-          thread.start();
-
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
-
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-
-
-  public class YieldExample {
-      public static void main(String[] args) {
-          Thread thread = new Thread(() -> {
-              for (int i = 0; i < 5; i++) {
-                  System.out.println("Thread is running: " + i);
-                  Thread.yield(); // Suggest the thread scheduler to yield execution
-              }
-          });
-
-          thread.start();
-
-          try {
-              thread.join(); // Wait for the thread to finish
-          } catch (InterruptedException e) {
-              Thread.currentThread().interrupt();
-          }
-
-          System.out.println("Main thread resumes after thread completion.");
-      }
-  }
-
-`;
+    await upload(cld, {
+      file: imageDetail,
+      options: options,
+      callback: (error, response) => {
+        console.log('ERROR ----> : ', error);
+        console.log('response ----> : ', response);
+      },
+    });
+  };
   return (
-    <ScrollView
+    <View
       style={{
         flex: 1,
-        backgroundColor: 'gray',
+        paddingHorizontal: 20,
       }}>
-      <ScrollView
-        horizontal={true}
-        style={{
-          minHeight: 10,
-          width: '100%',
-          backgroundColor: 'pink',
-          padding: moderateScale(20),
-          paddingHorizontal: moderateScale(20),
-        }}>
-        <Text
+      {image && (
+        <Image
+          source={{uri: image}}
           style={{
-            fontSize: moderateScale(15),
+            width: '100%',
+            height: 200,
+            borderRadius: moderateScale(5),
+            marginBottom: moderateScale(10),
+            // resizeMode: 'contain',
+          }}
+        />
+      )}
+
+      <View>
+        <TouchableOpacity
+          onPress={openImagePicker}
+          style={{
+            borderWidth: 1,
+            height: moderateScale(40),
+            padding: moderateScale(5),
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: moderateScale(5),
+            marginBottom: moderateScale(10),
           }}>
-          {code}
-        </Text>
-      </ScrollView>
-      <View
-        style={{
-          height: 500,
-        }}></View>
-    </ScrollView>
+          <Text
+            style={{
+              fontSize: moderateScale(20),
+              fontWeight: 'bold',
+            }}>
+            Add Image
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={uploadImage}
+          style={{
+            borderWidth: 1,
+            height: moderateScale(40),
+            padding: moderateScale(5),
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: moderateScale(5),
+            marginBottom: moderateScale(10),
+          }}>
+          <Text
+            style={{
+              fontSize: moderateScale(20),
+              fontWeight: 'bold',
+            }}>
+            Upload Image
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 export default ForTesting;
-
-const styles = StyleSheet.create({});
---
+++ src/service/cloudinary.js
@@ -0,0 +1,6 @@
+export // Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'demo',
+  },
+});
